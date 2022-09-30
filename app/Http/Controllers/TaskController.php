cdeb4f43d11489c036bd46361861598568b34e08
Phil <phil@harmonie.media>
2022-09-30 16:49:38 +0200

    Added Delete and Reload

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -105,6 +105,7 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return redirect()->back();
     }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,14 @@
 <x-app-layout>
 
+    <a href="{{ url('/') }}" class="flex justify-end mb-4 mr-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+
+    </a>
+
     <div class="flex items-center justify-between p-8 mb-8 bg-white shadow-xl rounded-xl">
 
         <div class="grid w-12 h-12 p-2 font-bold text-white bg-red-800 rounded-full place-content-center">
@@ -81,27 +90,45 @@
     </div>
 
     @forelse ($tasks as $task)
-        <div class="flex items-stretch justify-between mb-8 shadow-xl rounded-xl">
-            <div class="flex items-center justify-between w-full p-4 bg-white rounded-l-xl">
-                <div class="flex items-center">
-                    <div
-                        class="grid w-8 h-8 p-2 mr-2 font-bold text-white bg-red-800 rounded-full place-content-center">
-                        {{ Str::substr($task->user->name, 0, 1) }}
+        <div class="flex mb-8 space-x-4">
+            <div class="flex items-stretch justify-between flex-1 shadow-xl rounded-xl peer">
+                <div class="flex items-center justify-between w-full p-4 bg-white rounded-l-xl">
+                    <div class="flex items-center">
+                        <div
+                            class="grid w-8 h-8 p-2 mr-2 font-bold text-white bg-red-800 rounded-full place-content-center">
+                            {{ Str::substr($task->user->name, 0, 1) }}
+                        </div>
+                        <div class="font-bold">
+                            {{ $task->name }}
+                        </div>
                     </div>
-                    <div class="font-bold">
-                        {{ $task->name }}
+                    <div>
+                        {{ $task->date->format('d.m.Y') }}
                     </div>
-                </div>
-                <div>
-                    {{ $task->date->format('d.m.Y') }}
-                </div>
 
+                </div>
+                <div class="grid flex-1 p-2 font-bold text-white bg-red-800 rounded-r-xl place-content-center">
+                    +{{ $task->points }}
+                </div>
             </div>
-            <div class="grid flex-1 p-2 font-bold text-white bg-red-800 rounded-r-xl place-content-center">
-                +{{ $task->points }}
+
+            <div class="hidden text-white bg-red-800 rounded-md hover:bg-red-700 peer-hover:block hover:block">
+                <form method="post" action="{{ Route('tasks.destroy', $task) }}"
+                    class="flex items-center justify-center" onsubmit="return confirm('Sicher löschen?');">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+
+                    </button>
+                </form>
             </div>
+
         </div>
-
     @empty
         Keine Aufgaben vorhanden.
     @endforelse
